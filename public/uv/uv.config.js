7dffc07f998625f238856ec5e297966d1cc93fd2
nine4x <118857854+nine4x@users.noreply.github.com>
2023-05-22 09:32:06 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://uv.sorwn.org/',
+    bare: 'https://schooliscool.xyz/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
